21468f5c636e190bbc446202c505c3d46faa5bdc
Sai Khawn Kham <khawnkham222@gmail.com>
2025-05-04 13:14:23 +0630

    init: project init

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+   // images: {
+   //    domains: ["fakestoreapi.com"] // to use images from fakestoreapi.com
+   // },
+   // remotePatterns: {
+   //    domains: ["fakestoreapi.com"] // to use images from fakestoreapi.com
+   // }
+};
+
+export default nextConfig;
---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Container from "@/components/Container";
+import React from "react";
+
+const HomePage = () => {
+   return (
+      <Container>
+         <div>Home</div>
+      </Container>
+   );
+};
+
+export default HomePage;
